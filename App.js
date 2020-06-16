09bbea5375f35c93500c1bd90cc220bfe8d6171c
Maria Mathew <mariamathewc@gmail.com>
2020-06-15 21:31:58 -0700

    feat: Add new features to application
    helpers.js - Add daily matrics
    DisplaySlider - Add slider to app
    DisplayStepper - Add increment/decrement TextButton
    DateHeader - Add date to app
    AddEntry - Entrypoint invoked in App.js

---
+++ App.js
@@ -0,0 +1,20 @@
+//import * as React from 'react';
+import React from 'react';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+
+//export default function App() {
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View>
+          <AddEntry />
+        </View>
+      </Provider>
+    );
+  }
+}
